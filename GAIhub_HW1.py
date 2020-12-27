2c5aceabf1ce60ffd5e4cef0bb94897de92a5956
hilalsaygin <46250773+hilalsaygin@users.noreply.github.com>
2020-12-27 22:32:49 +0300

    Update GAIhub_HW1.py

--- GAIhub_HW1.py
+++ GAIhub_HW1.py
@@ -2,11 +2,10 @@
 values=list()
 for i in range(5):
     values.append(input("Enter Value {} : ".format(i+1)))
-for v in values:
-   print("Your {}th Value >> {} \nType of the value is {} ".format(values.index(v)+1,v,type(v)))
 
-"""
- ---- Detailed solution as follows; -----
+
+ #---- Detailed solution as follows; -----
+ 
  
 for v in values:
     if v.isdigit():
@@ -18,4 +17,9 @@ for v in values:
 
         except ValueError:
             print("Your {}th Value >> {} \nType of the value is {} ".format(values.index(v) + 1, v, type(v)))
+
+"""   Additionally input() method will always take the value from user as str type.
+for v in values:
+   print("Your {}th Value >> {} \nType of the value is {} ".format(values.index(v)+1,v,type(v)))
+   
 """
